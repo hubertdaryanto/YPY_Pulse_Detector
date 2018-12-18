cc103a01621d413a4b592d232301d096fedac077
Hubert <partishion@gmail.com>
2018-12-18 09:52:49 +0700

    Initial commit

---
+++ feature/src/main/java/com/yangpentingyakin/ypyheartearlydetection/feature/HeartRateDetection.java
@@ -0,0 +1,13 @@
+package com.yangpentingyakin.ypyheartearlydetection.feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeartRateDetection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_rate_detection);
+    }
+}
